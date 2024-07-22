09799e4580c8adc80c65185e14da756dfa06a6de
HarishvaranSK <posterno01@gmail.com>
2024-07-22 16:40:31 +0530

    Second Commit

--- src/App.js
+++ src/App.js
@@ -8,15 +8,31 @@ function App() {
   const [nav, setNav] = useState(false);
   const [selectedWeight, setSelectedWeight] = useState("400");
   const [text, setText] = useState("");
-  const [savedFont, setSavedFont] = useState("ABeeZee");
-  const [savedWeight, setSavedWeight] = useState("400");
+  const [savedFont, setSavedFont] = useState("");
+  const [savedWeight, setSavedWeight] = useState("");
 
   useEffect(() => {
+    
+    if(localStorage.getItem('font')){
+      setSelectedFont(localStorage.getItem('font'));
+    }
+    if(localStorage.getItem('weight')){
+      setSelectedWeight(localStorage.getItem('weight'));
+    }
+    if(localStorage.getItem('text')){
+      setText(localStorage.getItem('text'));
+    }
+    if(localStorage.getItem('nav')){
+      setNav(localStorage.getItem('nav'));
+    }
     setArr(Font);
   }, []);
 
   const handleSave = async () => {
-
+    localStorage.setItem('font', selectedFont);
+    localStorage.setItem('weight', selectedWeight);
+    localStorage.setItem('text', text);
+    localStorage.setItem('nav', true);
     await setSavedFont(selectedFont);
     await setSavedWeight(selectedWeight);
     setNav(true);
@@ -77,10 +93,13 @@ function App() {
           </textarea>
         </div>
         <div style={{ display: 'flex', alignItems: 'center', gap: '20px', justifyContent: 'center', marginTop: '20px' }}>
-          <button style = {{backgroundColor:'crimson', color : 'white', fontSize : '20px', borderRadius : '10px', cursor:'pointer', padding : '8px', border : '0px'}} onClick={() => { setText(""); setSelectedFont("ABeeZee"); setSelectedWeight("400"); setNav(false) }}>Reset</button>
+          <button style = {{backgroundColor:'crimson', color : 'white', fontSize : '20px', borderRadius : '10px', cursor:'pointer', padding : '8px', border : '0px'}} onClick={() => { setText(""); setSelectedFont("ABeeZee"); setSelectedWeight("400"); setNav(false); localStorage.removeItem('font', selectedFont);
+    localStorage.removeItem('weight');
+    localStorage.removeItem('text');
+    localStorage.removeItem('nav'); }}>Reset</button>
           <button style = {{backgroundColor:'crimson', color : 'white', fontSize : '20px', borderRadius : '10px', cursor:'pointer', padding : '8px', border : '0px'}} onClick={handleSave}>Save</button>
         </div>
-        {nav === true ?
+        {nav ?
           <div style={{ display: 'flex', alignItems: 'center', gap: '20px', justifyContent: 'center', fontSize: '20px' }}>The Entered Text with Font {savedFont} and style of {savedWeight} is  :<h1 style={styleVal}> {text}</h1></div> : ''
         }
       </center>
